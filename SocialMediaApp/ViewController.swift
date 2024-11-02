5668c1ccf68ab33361f9d8d2b516d2a786a1e565
Муса Зарифянов <zarifanovmusa@gmail.com>
2024-11-03 00:42:53 +0300

    Initial Commit

---
+++ SocialMediaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialMediaApp
+//
+//  Created by Муса Зарифянов on 03.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SocialMediaAppTests/SocialMediaAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SocialMediaAppTests.swift
+//  SocialMediaAppTests
+//
+//  Created by Муса Зарифянов on 03.11.2024.
+//
+
+import Testing
+@testable import SocialMediaApp
+
+struct SocialMediaAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
