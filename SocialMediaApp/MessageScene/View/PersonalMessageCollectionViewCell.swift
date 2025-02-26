ca66d2c10b4114d61ccc0cf178924c80c17b0582
Муса Зарифянов <zarifanovmusa@gmail.com>
2025-02-27 02:33:55 +0300

    Создание экрана личных сообщений part1

---
+++ SocialMediaApp/MessageScene/View/PersonalMessageCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PersonalMessageCollectionViewCell.swift
+//  SocialMediaApp
+//
+//  Created by Муса Зарифянов on 26.02.2025.
+//
+
+import UIKit
+
+class PersonalMessageCollectionViewCell: UICollectionViewCell {
+    
+}
